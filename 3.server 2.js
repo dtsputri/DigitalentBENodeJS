9814be7fb1e0d619809afd1af0a06150dcaef3e0
Putri <dtsputrirosimayasari@outlook.com>
2020-10-09 09:07:40 +0700

    menambahkan file server2

---
+++ 3.server 2.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+const server = http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write('HI, selamat data di <b>NodeJS</b> ');
+    res.end('<div>hi, selamat data di Node js</dib>');
+});
+
+server.listen(1000);
+console.log('server running on http://localhost:1000');
+
+// var http = require('http')
+
+// var server = http.createServer(function (req, res){
+//     res.end("hi, selamat data di node js");
+// });
+
+// server.listen(1000);
+// console.log("server running on http://localhost:1000");
